244659612200f97930b7f9d9e7cfa3a8cd33b9cd
Lucas de Morais Siqueira <lucas.morais.siqueira@gmail.com>
2017-01-09 16:35:08 -0200

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,47 +6,38 @@ mkdir /home/travis/sdl2install/bin
 export SDL_CONFIG="$SDL2DIR/bin"
 
 # install sdl2-2.0.5 
+cd ~
 wget https://www.libsdl.org/release/SDL2-2.0.5.tar.gz
-tar xf SDL2-2.0.5.tar.gz
-cd SDL2-2.0.5  
-./configure --prefix=$SDL2DIR
-make
-make install
-cd ..
+tar xf SDL2-2.0.5.tar. && cd SDL2-2.0.5  
+./autogen.sh && ./configure --prefix=$SDL2DIR
+make && make install
+
 
 # install SDL2_image-2.0.1
+cd ~
 wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz
-tar xf SDL2_image-2.0.1.tar.gz
-cd SDL2_image-2.0.1
-./configure --prefix=$SDL2DIR
-make
-make install
-cd ..
+tar xf SDL2_image-2.0.1.tar.gz && cd SDL2_image-2.0.1
+./autogen.sh && ./configure --prefix=$SDL2DIR
+make && make install
 
 # install SDL2_ttf-2.0.14
+cd ~
 wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
-tar xf SDL2_ttf-2.0.14.tar.gz
-cd SDL2_ttf-2.0.14
-./configure --prefix=$SDL2DIR
-make
-make install
-cd ..
+tar xf SDL2_ttf-2.0.14.tar.gz && cd SDL2_ttf-2.0.14
+./autogen.sh && ./configure --prefix=$SDL2DIR
+make && make install
 
 # install SDL2_mixer-2.0.1
+cd ~
 wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz
-tar xf SDL2_mixer-2.0.1.tar.gz
-cd SDL2_mixer-2.0.1
-./configure --prefix=$SDL2DIR
-make
-make install
-cd ..
+tar xf SDL2_mixer-2.0.1.tar.gz && cd SDL2_mixer-2.0.1
+./autogen.sh && ./configure --prefix=$SDL2DIR
+make && make install
 
 # install SDL2_gfx-1.0.1
+cd ~
 wget http://www.ferzkopp.net/Software/SDL2_gfx/SDL2_gfx-1.0.1.tar.gz
-tar xf SDL2_gfx-1.0.1.tar.gz
-cd SDL2_gfx-1.0.1
-./configure --prefix=$SDL2DIR
-make
-make install
-cd ..
+tar xf SDL2_gfx-1.0.1.tar.gz && cd SDL2_gfx-1.0.1
+./autogen.sh && ./configure --prefix=$SDL2DIR
+make && make install
 
